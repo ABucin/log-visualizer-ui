b5cbe7b0284d4b34a3a1953edfcdb86267a8ee43
Andrei Bucin <andrei@2-create.ch>
2018-11-18 18:04:06 +0200

    Added Font Awesome icons.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<b-nav vertical class="w-25">
+			<b-nav-item active>Visualize data</b-nav-item>
+			<b-nav-item>Generate reports <font-awesome-icon icon="user-secret" /></b-nav-item>
+		</b-nav>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import bNav from 'bootstrap-vue/es/components/nav/nav';
+	import bNavItem from 'bootstrap-vue/es/components/nav/nav-item';
+	import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			bNav,
+			bNavItem,
+			FontAwesomeIcon,
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
+library.add(faUserSecret);
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
